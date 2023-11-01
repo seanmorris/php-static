08d1400398a2dc9d8b41de651c3ba954b11a9ebe
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-11-01 03:40:25 -0400

    db test

--- db-test.php
+++ db-test.php
@@ -1,31 +1,25 @@
 <?php
 
-$db  = (new Vrzno)->env->db;
 $get = vrzno_env('_GET');
+$db  = vrzno_env('db');
 
 if(!$get->id)
 {
-    $statement = $db->prepare('SELECT * FROM Customers');
+    $statement = $db
+    ->prepare('SELECT * FROM Customers');
 }
 else
 {
-    $statement = $db->prepare('SELECT * FROM Customers WHERE CustomerId = ?')
+    $statement = $db
+    ->prepare('SELECT * FROM Customers WHERE CustomerId = ?')
     ->bind($get->id);
 }
 
 $statement->all()->then(function($result){
-    $headers = false;
     $records = (array)$result->results;
-    $stdout  = fopen('php://stdout', 'w');
 
     foreach($records as $record)
     {
         var_dump($record);
-        // if(!$headers)
-        // {
-        //     fputcsv($stdout, array_keys($record), "\t");
-        // }
-
-        // fputcsv($stdout, $record, "\t");
     }
 });
