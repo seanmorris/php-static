d2d092dd952b2eadb92644b8c451cb467f092cb2
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-11-01 02:36:56 -0400

    db test

--- db-test.php
+++ db-test.php
@@ -14,21 +14,8 @@ $statement = $db
 
 $all = vrzno_await($statement->all());
 
-$out = fopen('php://stdout', 'w');
-$headers = false;
+$all->then(function($all){
+    
+    var_dump($all->results);
 
-$all->then(function($all) use($out, &$headers) {
-    foreach($all->results as $record)
-    {
-        var_dump($record);
-        var_dump((array)$record);
-
-        if(!$headers)
-        {
-            fputcsv($out, array_keys((array)$record), "\t");
-            $headers = true;
-        }
-        
-        fputcsv($out, (array)$record, "\t");
-    }
 });
