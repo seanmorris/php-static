09ce5515ca100bd871e0b5229384f61b2d816e18
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-11-01 01:14:13 -0400

    db test

---
+++ db-test.php
@@ -0,0 +1,14 @@
+<?php
+
+$vrzno = new Vrzno;
+
+$db = $vrzno->env->db;
+
+$statement = $db
+    ->prepare('SELECT * FROM Customers WHERE CompanyName = ?')
+    ->bind('Bs Beverages')
+    ->all();
+
+$statement->then(function($result){
+    echo json_encode($result);
+});
