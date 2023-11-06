8a34a56f53601de7da8c6071e0d01b72876ce176
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-11-06 18:48:12 -0500

    Static origin

--- wiki-frontend.php
+++ wiki-frontend.php
@@ -1,9 +1,9 @@
 <?php
-eval('?>'.vrzno_await( vrzno_await((new Vrzno)->fetch('http://localhost:8081/CloudAutoloader.php'))->text()));
+eval('?>'.vrzno_await( vrzno_await((new Vrzno)->fetch(vrzno_env('staticOrigin') . '/CloudAutoloader.php'))->text()));
 
 CloudAutoloader::register('http://localhost:8788/zip-proxy?zip=');
 
-eval('?>'.vrzno_await( vrzno_await((new Vrzno)->fetch('http://localhost:8081/WikiMarkdown.php'))->text()));
+eval('?>'.vrzno_await( vrzno_await((new Vrzno)->fetch(vrzno_env('staticOrigin') . '/WikiMarkdown.php'))->text()));
 
 $parser = new WikiMarkdown();
 
